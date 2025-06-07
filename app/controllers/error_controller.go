a28a9558fc79c1704786794fbf862a95d7a15802
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2025-06-07 13:15:37 -0500

    agregando pruebas de serivicios REST

--- app/controllers/error_controller.go
+++ app/controllers/error_controller.go
@@ -1,9 +1,20 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func Error404(c *gin.Context) {
-	c.HTML(404, "error.tmpl", gin.H{})
+	if c.Request.Method == http.MethodGet {
+		c.HTML(http.StatusNotFound, "error.tmpl", gin.H{
+			"title": "Página no encontrada",
+		})
+	} else {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error":  "Recurso no encontrado",
+			"detail": c.Request.Method + "-" + c.Request.URL.Path,
+		})
+	}
 }
