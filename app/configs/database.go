78a039ca89165f94a54f4fbbbe81cc2b8b2b7612
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2025-06-24 15:58:50 -0500

    usando .env para la cadena de conexion a ld DB

--- app/configs/database.go
+++ app/configs/database.go
@@ -1,6 +1,10 @@
 package configs
 
 import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -8,10 +12,20 @@ import (
 var DB *gorm.DB
 
 func ConnectToDB() error {
-	var err error
-	DB, err = gorm.Open(sqlite.Open("db/app.db"), &gorm.Config{})
+	err := godotenv.Load()
 	if err != nil {
-		return err
+		log.Println("No se pudo cargar .env, usando variables del sistema")
+	}
+
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("La variable DATABASE_URL no está definida en .env")
+	}
+
+	var err2 error
+	DB, err2 = gorm.Open(sqlite.Open(dbURL), &gorm.Config{})
+	if err2 != nil {
+		return err2
 	}
 	return nil
 }
